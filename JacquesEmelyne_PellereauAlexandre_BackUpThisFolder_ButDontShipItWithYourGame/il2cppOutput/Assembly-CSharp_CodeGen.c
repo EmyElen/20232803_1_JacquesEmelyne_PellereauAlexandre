8bb2ab228800bb8900f62f4a20ff757e0e961ed4
Emelyne Jacques <emelyne.jacques@epita.fr>
2023-03-28 12:04:46 +0200

    ajout de l'apk

---
+++ JacquesEmelyne_PellereauAlexandre_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,189 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000011 System.Void ApplicationManager::SpawnEnemy()
+extern void ApplicationManager_SpawnEnemy_m3CF5F0A8625C3335F42A9A0AB87B18AD45837EC6 (void);
+// 0x00000012 System.Void ApplicationManager::Start()
+extern void ApplicationManager_Start_m7BDE5D7E870C475BA6E0EAF0D8DA78851DA276A7 (void);
+// 0x00000013 System.Void ApplicationManager::Update()
+extern void ApplicationManager_Update_mDC8754BF6C527F98AD1AE6E98D53F2A142ECBFA1 (void);
+// 0x00000014 System.Void ApplicationManager::.ctor()
+extern void ApplicationManager__ctor_m9728432FD921373C75D5AD1BB10C1C7BB926E45F (void);
+// 0x00000015 System.Void Game::OnTouch()
+extern void Game_OnTouch_m1EF9F31BB939128BB7191B65FEC23DD705782C4B (void);
+// 0x00000016 System.Void Game::Start()
+extern void Game_Start_m81FFBF966FD86236D5383C0295E70170EAE3F1DB (void);
+// 0x00000017 System.Void Game::Update()
+extern void Game_Update_mA1557241B926376A448DF2A57135D7BD10A5A51F (void);
+// 0x00000018 System.Void Game::Spawn()
+extern void Game_Spawn_m4F3C59230DE38BC2C77899BC483863644060479D (void);
+// 0x00000019 System.Void Game::KillingEnemy()
+extern void Game_KillingEnemy_mD65EEB9AC74C8C20DD84EEB3BD96FE262A31281E (void);
+// 0x0000001A System.Void Game::PlayAgain()
+extern void Game_PlayAgain_m1DC3875613040786DD2C1B18761B1286BF50071E (void);
+// 0x0000001B System.Void Game::EndGame()
+extern void Game_EndGame_mBB6853080D3186DF8D5AE48B1C534B309713A371 (void);
+// 0x0000001C System.Void Game::.ctor()
+extern void Game__ctor_m22BD482E0EC7CE99133FCF5EEF21DBC98A0BE5C3 (void);
+// 0x0000001D System.Void KillingCam::Start()
+extern void KillingCam_Start_mDC8F64D429D49E0D6454A6E5897F8B7BAC5F1FB2 (void);
+// 0x0000001E System.Void KillingCam::Update()
+extern void KillingCam_Update_m197C26270DC6695D1607669BA9C3738A74BDFFB2 (void);
+// 0x0000001F System.Void KillingCam::.ctor()
+extern void KillingCam__ctor_mB7BA6444F33888589C797448B7FAD2D58391FC79 (void);
+// 0x00000020 System.Void Scene2Manager::OnTouch()
+extern void Scene2Manager_OnTouch_mA74E78A6FED93632C2D54895D348AF447C46266A (void);
+// 0x00000021 System.Void Scene2Manager::InstantiateObject(UnityEngine.Vector3,UnityEngine.Quaternion)
+extern void Scene2Manager_InstantiateObject_m7A87566E230B0B6131C28A282004E90E7A6226A1 (void);
+// 0x00000022 System.Void Scene2Manager::Start()
+extern void Scene2Manager_Start_m74521096B1209D370C3C2CB5C2C561E5FF267C95 (void);
+// 0x00000023 System.Void Scene2Manager::Update()
+extern void Scene2Manager_Update_mBF7D429302B1F036B07C8D42A74863C95FE4F003 (void);
+// 0x00000024 System.Void Scene2Manager::ChangeColor()
+extern void Scene2Manager_ChangeColor_mF6ED35312AA8E3BB525B05355088049C45734E12 (void);
+// 0x00000025 System.Void Scene2Manager::.ctor()
+extern void Scene2Manager__ctor_m09641AE6D21607BC9AFFCB01C29BF63B21F978CB (void);
+static Il2CppMethodPointer s_methodPointers[37] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	ApplicationManager_SpawnEnemy_m3CF5F0A8625C3335F42A9A0AB87B18AD45837EC6,
+	ApplicationManager_Start_m7BDE5D7E870C475BA6E0EAF0D8DA78851DA276A7,
+	ApplicationManager_Update_mDC8754BF6C527F98AD1AE6E98D53F2A142ECBFA1,
+	ApplicationManager__ctor_m9728432FD921373C75D5AD1BB10C1C7BB926E45F,
+	Game_OnTouch_m1EF9F31BB939128BB7191B65FEC23DD705782C4B,
+	Game_Start_m81FFBF966FD86236D5383C0295E70170EAE3F1DB,
+	Game_Update_mA1557241B926376A448DF2A57135D7BD10A5A51F,
+	Game_Spawn_m4F3C59230DE38BC2C77899BC483863644060479D,
+	Game_KillingEnemy_mD65EEB9AC74C8C20DD84EEB3BD96FE262A31281E,
+	Game_PlayAgain_m1DC3875613040786DD2C1B18761B1286BF50071E,
+	Game_EndGame_mBB6853080D3186DF8D5AE48B1C534B309713A371,
+	Game__ctor_m22BD482E0EC7CE99133FCF5EEF21DBC98A0BE5C3,
+	KillingCam_Start_mDC8F64D429D49E0D6454A6E5897F8B7BAC5F1FB2,
+	KillingCam_Update_m197C26270DC6695D1607669BA9C3738A74BDFFB2,
+	KillingCam__ctor_mB7BA6444F33888589C797448B7FAD2D58391FC79,
+	Scene2Manager_OnTouch_mA74E78A6FED93632C2D54895D348AF447C46266A,
+	Scene2Manager_InstantiateObject_m7A87566E230B0B6131C28A282004E90E7A6226A1,
+	Scene2Manager_Start_m74521096B1209D370C3C2CB5C2C561E5FF267C95,
+	Scene2Manager_Update_mBF7D429302B1F036B07C8D42A74863C95FE4F003,
+	Scene2Manager_ChangeColor_mF6ED35312AA8E3BB525B05355088049C45734E12,
+	Scene2Manager__ctor_m09641AE6D21607BC9AFFCB01C29BF63B21F978CB,
+};
+static const int32_t s_InvokerIndices[37] = 
+{
+	5105,
+	4149,
+	5200,
+	5200,
+	5200,
+	5200,
+	8270,
+	5146,
+	4182,
+	5200,
+	5200,
+	5200,
+	4044,
+	4149,
+	5200,
+	8270,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	5200,
+	2300,
+	5200,
+	5200,
+	5200,
+	5200,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	37,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
